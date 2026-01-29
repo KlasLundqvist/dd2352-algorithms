a4adda357664e03048ede08a81990309374cb974
Klas Lundqvist <klalun@kth.se>
2026-01-29 11:31:11 +0100

    ..

---
+++ Sequence.java
@@ -0,0 +1,28 @@
+public class Sequence {
+
+
+    //looking for the max length of a subsequence
+    // LIS[n] = 1 + max{LIS[k] | k < n, A[k] < A[n]}
+
+    public static int lis (int[] a)
+    {
+        int[] l =  1 * a.length;
+
+        for (int i = 1; i < a.length; i++)
+        {
+
+        }
+
+        return l;
+
+    }
+
+
+    
+
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
